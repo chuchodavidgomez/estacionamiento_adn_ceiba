4542340efe1756e52df3567dfcf2eca1cbe7f4d9
Jesús David Gómez Osorno - Ceiba Software <jesus.gomez@ceiba.com.co>
2019-06-04 09:38:02 -0500

    database corregida

---
+++ parqueadero/src/main/resources/db/migration/DDL/V1.0__schemas.sql
@@ -0,0 +1,20 @@
+CREATE TABLE vehiculo (
+  placa VARCHAR(6) NOT NULL,
+  tipo VARCHAR(5) NOT NULL,
+  cilindraje INT NOT NULL,
+  marca VARCHAR(45) NOT NULL,
+  modelo VARCHAR(45) NOT NULL,
+  fechaIngreso datetime null,
+  PRIMARY KEY (placa)
+);
+
+CREATE TABLE historial(id INT NOT NULL AUTO_INCREMENT,
+  fechaIngreso DATETIME NOT NULL,
+  fechaSalida DATETIME NULL,
+  pago FLOAT NULL,
+  placaVehiculo VARCHAR(6) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (placaVehiculo) REFERENCES vehiculo(placa)
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE
+);
